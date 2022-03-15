0af402455803af81a87d1742865c145d6735f7c1
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2022-03-15 08:49:58 +0600

    ./

---
+++ practice/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/"],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        container: {},
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
