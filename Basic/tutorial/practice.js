3f592cb21f64002f5d6a5fb1f4cc0dd4f544e0b6
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2022-04-06 02:27:17 +0600

    Making a easy tutorial with javascript

---
+++ Basic/tutorial/practice.js
@@ -0,0 +1,18 @@
+document.getElementById('innerHtml').innerHTML = "This is InnerHtml";
+
+// head section
+function HeadFunction() {
+    document.getElementById("head").innerHTML = "Head is changed after clicking the button";
+}
+// body section
+
+function BodyFunction() {
+    document.getElementById("BodyFunction").innerHTML = "Body is changed after clicking the button";
+}
+
+//----JavascriptOutput
+// using innerHTML
+
+document.getElementById("InnderHTML").innerHTML = 5 + 6;
+// using document.write()----------------------------------------------------------------
+// document.write(5 + 6);
